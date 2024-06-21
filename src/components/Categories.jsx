aa7f9b10a9947253dfda5734aace9dd6e700d4a1
thatsmanmeet <120583077+thatsmanmeet@users.noreply.github.com>
2024-06-21 05:32:54 +0530

    fix category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,30 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import { FaArrowRight , FaArrowLeft } from "react-icons/fa";
-
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
 export default function Categories() {
-    const [Categories , setcategory]=useState([]);
-    const fetchcategory =async() => {
-const response = await fetch("http://localhost:6000/categories");
-const data = await response.json();
-setcategory(data);
+    const [categories, setCategories] = useState([]);
+
+    const fetchCategories = async () => {
+        const response = await fetch("./data/category.json");
+        const data = await response.json();
+        setCategories(data);
     }
-    useEffect(
-        ()=>{
-            fetchcategory();
-        },[]
-    )
-    return(
-    
-        <div className='max-w-[1200px] mx-auto  items-center'>
-            <div className='flex items-center justify-between'>    
-        <div className='text-[25px] font-bold'>  What's on your mind? </div>
-        <div className='flex'> 
-            <div className='cursor-pointer flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2'><FaArrowLeft /></div>
-            <div className='cursor-pointer flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2'><FaArrowRight /></div>
-    </div>
-    </div>
-    </div>
-  
-  )
+
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
+    return (
+        <div className='max-w-[1200px] mx-auto items-center'>
+            <div className='flex items-center justify-between'>
+                <div className='text-[25px] font-bold'>What's on your mind?</div>
+                <div className='flex'>
+                    <div className='cursor-pointer flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2'>
+                        <FaArrowLeft />
+                    </div>
+                    <div className='cursor-pointer flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2'>
+                        <FaArrowRight />
+                    </div>
+                </div>
+            </div>
+            <div className='grid grid-cols-4 gap-4 mt-4'>
+                {categories.map((category, index) => (
+                    <div key={index} className='text-center'>
+                        <img src={category.image} alt={category.path} className='w-full h-auto' />
+                        <p>{category.path.replace(/-/g, ' ')}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
