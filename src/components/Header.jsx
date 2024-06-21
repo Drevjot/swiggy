fce350499f0c55a0bf525efaa3ef8c0918acf5f4
thatsmanmeet <120583077+thatsmanmeet@users.noreply.github.com>
2024-06-21 11:09:09 +0530

    change v3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import { IoPersonSharp } from "react-icons/io5";
 import { FaCartShopping } from "react-icons/fa6";
 export default function Header() {
     const [toggle,setToggle]= useState(false);
+    const [loginMode,setLoginMode] = useState(true)
    const showsidemenu = (e) => {
     e.preventDefault();
     console.log('hi');
@@ -62,15 +63,23 @@ export default function Header() {
             right:toggle? '0%' : '-100%'
         }}>
             <p className='text-[1.5rem] text-slate-500 cursor-pointer' onClick={hidesidemenu}>&#10005;</p>
-            <h2 className='text-2xl mt-[3.5rem] ml-4 font-bold'>Login</h2>
+            <h2 className='text-2xl mt-[3.5rem] ml-4 font-bold'>{loginMode ? 'Login' : 'Sign Up'}</h2>
             <div className='ml-4'>
                 <span className='text-sm'>or </span>
-            <span className='text-sm text-[#fc8019]'>create an account</span>
-            <hr className='w-[40px] mt-4 mb-6 divide-black'/>
-            <input type="number" className='p-5 border border-1 w-[80%]' placeholder='Phone Number' />
+            <span className='text-sm text-[#fc8019] cursor-pointer' onClick={()=>setLoginMode(!loginMode)}>{loginMode ? 'create an account' : 'login to your account'}</span>
+            <hr className='w-[40px] mt-4 mb-6 divide-black'/> 
+            {loginMode ? (
+                  <input type="number" className='p-5 border border-1 w-[80%]' placeholder='Phone Number' />
+            ) : (
+                <>
+                <input type="number" className='p-5 border border-1 w-[80%]' placeholder='Phone Number' />
+                <input type="text" className='p-5 border border-1 w-[80%]' placeholder='Name' />
+                <input type="email" className='p-5 border border-1 w-[80%]' placeholder='Email' />
+                </>
+            )}
             <img src='./images/swiggymenu.avif' className='w-24 absolute top-[4rem] right-[8rem]'/>
             <div className='mt-5'>
-                <button className='w-[80%] bg-[#fc8019] p-5 text-white font-bold'>LOGIN</button>
+                <button className='w-[80%] bg-[#fc8019] p-5 text-white font-bold'>{loginMode ? 'LOGIN' : 'CONTINUE'}</button>
                 <p className='font-bold mt-2 text-[10px]'>
                     By clicking on Login, I accept the Terms & Conditions & Privacy Policy
                 </p>
